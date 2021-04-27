e0139997c539aadce0f5c71b7fafad77de05af50
nturinski <naturins@microsoft.com>
2021-04-27 15:58:28 -0700

    Oh Im triggered

--- app/src/index.js
+++ app/src/index.js
@@ -10,40 +10,41 @@ import axios from 'axios';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 class FetchDemo extends React.Component {
-    state = {
-      userName: ''
-    }
-  
-    componentDidMount() {
-      axios.get(`/api/${this.props.subreddit}`)
-        .then(res => {
-          console.log(res.data);
-          var userName = 'Hey ' + res.data.userDetails + '!';
-          this.setState({userName});
-        });
-    }
-  
-    render() {
-      return (
-        <div className="App">
+  state = {
+    userName: ''
+  }
+
+  componentDidMount() {
+    axios.get(`/api/${this.props.subreddit}`)
+      .then(res => {
+        console.log(res.data);
+        var userName = 'Hey ' + res.data.userDetails + '!';
+        this.setState({ userName });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             If you login with Github below, I'll say hi :)
+            Oh, I'm triggered.
           </p>
           <a className="App-link" href="/login"> Login with Github </a>
           <a className="App-link" href="/logout"> Logout</a>
           <p>{this.state.userName}</p>
         </header>
       </div>
-      );
-    }
+    );
   }
-  
-  ReactDOM.render(
-    <FetchDemo subreddit="getList"/>,
-    document.getElementById('root')
-  );
+}
+
+ReactDOM.render(
+  <FetchDemo subreddit="getList" />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
